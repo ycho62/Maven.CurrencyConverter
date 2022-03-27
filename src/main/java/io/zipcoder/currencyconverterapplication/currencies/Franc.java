00fbd9ef3f7b94bc9a13d7f2236defe6effd329c
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-27 00:47:05 -0400

    final

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/Franc.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/Franc.java
@@ -1,6 +1,10 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class Franc implements ConvertableCurrency {
+    public CurrencyType getCurrencyType(){
+        return CurrencyType.FRANC;
+    }
 }
